99924b6cf0d96718957b0b20b06b09bcc11a509f
LikhithaBG <130587942+LikhithaBG@users.noreply.github.com>
2023-06-07 12:02:48 +0530

    Add files via upload

---
+++ Admin_login.sql
@@ -0,0 +1,18 @@
+create database lifestyle;
+
+
+CREATE TABLE Admin_login ( 
+AdminID INT IDENTITY(1,1) PRIMARY KEY,
+FirstName VARCHAR(50) NOT NULL, 
+LastName VARCHAR(50) NOT NULL,
+Email VARCHAR(100) NOT NULL,
+Password VARCHAR(50) NOT NULL,
+CONSTRAINT CHK_Admin_Email CHECK (Email LIKE '%@gmail.com') );
+
+
+INSERT INTO Admin_login(FirstName, LastName, Email, Password) VALUES 
+('Divyashree', 'C', 'Divyashreec@gmail.com','123'),
+('Syed', 'Ayan', 'ayan@gmail.com','123'), 
+('Likhitha', 'B G', 'likhitha@gmail.com','123');
+
+select * from Admin_login;
